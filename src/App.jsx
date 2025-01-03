b16e3d06ed02ffebcf277a3fe89d35fdaa130af9
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-01-03 20:10:28 +0900

    feat: 채널페이지 제작작

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/Home";
 import MySubscribePage from "./pages/MySubcribe/MySubscribe";
+import ChannelPage from "./pages/Channel/Channel"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/mypage/subscribtions" element={<MySubscribePage />} />
+        <Route path="/@:channelId" element={<ChannelPage />} />
       </Routes>
     </Router>
   );
