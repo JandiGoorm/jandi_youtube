11942278ea1011512bf186174881bd1ff9678894
한정희 <jeo4924@hanjeonghuiui-MacBookPro.local>
2025-01-06 23:01:02 +0900

    Feat: 무한스크롤 구현

--- src/pages/Home/Videos/Videos.jsx
+++ src/pages/Home/Videos/Videos.jsx
@@ -1,31 +1,49 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import YoutubeService from "../../../apis/youtube";
 import styles from "./Videos.module.css";
 
 function Videos() {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]); // 비디오 데이터
+  const [page, setPage] = useState(1); // 현재 페이지
+  const [isLoading, setIsLoading] = useState(false); // 로딩 상태
+  const observerTarget = useRef(null); // 관찰 대상
 
   useEffect(() => {
     const fetchVideos = async () => {
+      setIsLoading(true);
       try {
-        // const response = await api.get('/search', {
-        //     params: {
-        //       part: "snippet",
-        //       chart: "mostPopular",
-        //       type: "video",
-        //       regionCode:"KR",
-        //       maxResults: 10,
-        //     },
-        //   });
-        const response = await YoutubeService.fetchVideos();
-        setVideos(response.data.items);
+        const response = await YoutubeService.fetchVideos(page);
+        setVideos((prevVideos) => [...prevVideos, ...response.data.items]);
       } catch (error) {
         console.error("Error fetching videos:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
-
     fetchVideos();
-  }, []);
+  }, [page]);
+
+  //옵저버 설정
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && !isLoading) {
+          setPage((prevPage) => prevPage + 1); // 페이지 증가
+        }
+      },
+      { threshold: 0.5 } // 대상이 100% 보일 때 실행
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [isLoading]);
 
   return (
     <div>
@@ -46,6 +64,7 @@ function Videos() {
           </li>
         ))}
       </ul>
+      <div ref={observerTarget} className={styles.loading}></div>
     </div>
   );
 }
