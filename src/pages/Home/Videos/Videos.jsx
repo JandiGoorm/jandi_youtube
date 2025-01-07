dffb783bed224ed5df553759e464dc0b04b508e5
한정희 <jeo4924@hanjeonghuiui-MacBookPro.local>
2025-01-07 16:15:22 +0900

    Feat: 동영상 썸네일 클릭시 해당 동영상 페이지로 이동

--- src/pages/Home/Videos/Videos.jsx
+++ src/pages/Home/Videos/Videos.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import YoutubeService from "../../../apis/youtube";
+import {formatISO} from "../../../utils/date.js"
 import styles from "./Videos.module.css";
 
 function Videos() {
@@ -9,6 +10,7 @@ function Videos() {
   const observerTarget = useRef(null); // 관찰 대상
 
   useEffect(() => {
+    //데이터 로드
     const fetchVideos = async () => {
       setIsLoading(true);
       try {
@@ -26,17 +28,17 @@ function Videos() {
       }
     };
     fetchVideos();
+    console.log(videos);
   }, [page]);
 
-  //옵저버 설정
+  //옵저버
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting && !isLoading) {
-          setPage((prevPage) => prevPage + 1); // 페이지 증가
-        }
-      },
-      { threshold: 0.5 } // 대상이 100% 보일 때 실행
+          setPage((prevPage) => prevPage + 1); 
+      }},
+      { threshold: 0.5 }
     );
 
     if (observerTarget.current) {
@@ -50,6 +52,12 @@ function Videos() {
     };
   }, [isLoading]);
 
+  //비디오 클릭시 해당 비디오로 이동
+  const videoOnClick = (videoId) => {
+    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    window.location.href = videoUrl;
+  }
+
   return (
     <div>
       <ul className={styles.video_list}>
@@ -59,12 +67,22 @@ function Videos() {
               className={styles.video_thumbnail}
               src={video.snippet.thumbnails.medium.url}
               alt={video.snippet.title}
+              onClick={()=> videoOnClick(video.id.videoId)}
             />
-            <div className={styles.video_description}>
+            <div className={styles.video_information}>
+              <img
+                className={styles.video_channel_img}
+                src="https://yt3.ggpht.com/yti/ANjgQV9PAjI-WMfy-zSByztHi-Gw0cX2ORxDdilHqSZCmhaR8w=s108-c-k-c0x00ffffff-no-rj"
+              />
+              <div>
               <p className={styles.video_title}>{video.snippet.title}</p>
-              <p className={styles.video_channeltitle}>
-                {video.snippet.channelTitle}
-              </p>
+              <p className={styles.video_channeltitle}>{video.snippet.channelTitle}</p>
+                <div className={styles.video_statistics}>
+                  <p className={styles.video_view_count}></p>
+                  <p className={styles.video_published_time}>{formatISO(video.snippet.publishTime)}</p>
+                </div>
+              </div>
+
             </div>
           </li>
         ))}
