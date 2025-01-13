82c0c47d9f5244a513a7277108918ebba8d27fc8
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-01-14 00:50:00 +0900

    feat: 채널 메인페이지 최신목록 추가

--- src/pages/Channel/ChannelSections/HomeComponent/RecentPlayList.jsx
+++ src/pages/Channel/ChannelSections/HomeComponent/RecentPlayList.jsx
@@ -1,29 +1,62 @@
 import React, { useState, useEffect } from "react";
 import YoutubeService from "../../../../apis/youtube";
+import styles from "./RecentPlayList.module.css";
+import { formatHitCount } from "../../../../utils/hit";
+import { formatDuration } from "../../../../utils/time";
+import { formatISO } from "../../../../utils/date";
 
 const RecentPlayList = ({section}) => {
+  const [videos, setVideos] = useState([]);
 
   const fetchPlayList = async(playlistId) => {
     try{
       console.log(playlistId);
-      const response = await YoutubeService.fetchPlaylistItems({
-        part: "snippet,contentDetails",
-        maxResults: 10,
-        playlistId: playlistId,
+      const response = await YoutubeService.fetchSearch({
+        part: "snippet",
+        type: "video",
+        channelId: playlistId,
+        regionCode: "KR",
+        order: "date",
+        maxResults: 12,
       });
-        console.log(response);
+      const videoIds = response.data.items.map((item) => item.id.videoId);
+
+      const videoDetailsResponse = await YoutubeService.fetchVideos({
+        part: "contentDetails,snippet,statistics",
+        id: videoIds.join(","),
+      });
+
+      setVideos(videoDetailsResponse.data.items);
     }catch(error){
         console.log("error: "+ error);
       }
 }
 
 useEffect (()=> {
-    fetchPlayList(section.id);
+    fetchPlayList(section.snippet.channelId);
   },[]);
 
   return (
     <div>
-        <h3>{section.id}</h3>
+      <h1>동영상</h1>
+      <ul className={styles.video_list}>
+                  {videos.map((video) => (
+                    <li className={styles.video_item} key={video.id}>
+                      <div>
+                      <img
+                        className={styles.video_thumbnail}
+                        src={video.snippet.thumbnails.medium.url}
+                        alt={video.snippet.localized.title}
+                      />
+                      <p className={styles.video_duration}>{formatDuration(video.contentDetails.duration)}</p>
+                      </div>
+                      <div className={styles.video_description}>
+                        <p className={styles.video_title}>{video.snippet.localized.title}</p>
+                        <p className={styles.video_sub}>{formatHitCount(video.statistics.viewCount)}﹒{formatISO(video.snippet.publishedAt)}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
   </div>
   );
 };
