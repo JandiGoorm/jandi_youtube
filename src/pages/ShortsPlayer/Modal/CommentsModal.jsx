246873599c87142d3fe1e146c33b4e708a0b68ed
한정희 <jeo4924@hanjeonghuiui-MacBookPro.local>
2025-01-14 23:06:05 +0900

    Feat: 댓글 모달창 내 필터 버튼 클릭 시 정렬 기준 모달창이 뜨도록 구현

--- src/pages/ShortsPlayer/Modal/CommentsModal.jsx
+++ src/pages/ShortsPlayer/Modal/CommentsModal.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import styles from "./CommentsModal.module.css"; 
+import React, { useState } from "react";
+import styles from "./CommentsModal.module.css";
 import { RiFilter2Fill } from "react-icons/ri";
 import { TfiClose } from "react-icons/tfi";
 
+const CommentModal = ({ isOpen, onClose, children }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isSortByPopularity, setIsSortByPopularity] = useState(true); //true: 인기순, false: 최신순
 
-const Modal = ({ isOpen, onClose, children }) => {
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const setSortByPopularity = () => {
+    setIsSortByPopularity(true);
+    setIsDropdownOpen(false);
+    console.log("인기순 정렬 선택");
+  }
+  
+  const setSortByLatest = () => {
+    setIsSortByPopularity(false);
+    setIsDropdownOpen(false);
+    console.log("최신순 정렬 선택");
+  }
+
   if (!isOpen) return null; // 모달이 열리지 않았으면 렌더링하지 않음
 
   return (
@@ -12,7 +30,15 @@ const Modal = ({ isOpen, onClose, children }) => {
       <div className={styles.modalContent}>
         <header>
           <p>댓글</p>
-          <button ><RiFilter2Fill /></button>
+          <div className={styles.filterContainer}>
+            <button onClick={toggleDropdown}><RiFilter2Fill /></button>
+            {isDropdownOpen && (
+              <div className={styles.dropdownMenu}>
+                <button onClick={setSortByPopularity}>인기 댓글순</button>
+                <button onClick={setSortByLatest}>최신순</button>
+              </div>
+            )}
+          </div>
           <button onClick={onClose}><TfiClose /></button>
         </header>
         {children}
@@ -21,4 +47,4 @@ const Modal = ({ isOpen, onClose, children }) => {
   );
 };
 
-export default Modal;
+export default CommentModal;
