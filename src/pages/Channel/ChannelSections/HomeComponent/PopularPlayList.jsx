6c7eadeaaf730b7c131a082412d96d4aae727a8c
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-01-14 23:57:07 +0900

    feat: 채널페이지 인기 동영상 목록 추가가

---
+++ src/pages/Channel/ChannelSections/HomeComponent/PopularPlayList.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import YoutubeService from "../../../../apis/youtube";
+import styles from "./PopularPlayList.module.css";
+import { formatHitCount } from "../../../../utils/hit";
+import { formatDuration } from "../../../../utils/time";
+import { formatISO } from "../../../../utils/date";
+
+const PopularPlayList = ({section}) => {
+  const [videos, setVideos] = useState([]);
+
+  const fetchPlayList = async(playlistId) => {
+    try{
+      console.log(playlistId);
+      const response = await YoutubeService.fetchSearch({
+        part: "snippet",
+        type: "video",
+        channelId: playlistId,
+        regionCode: "KR",
+        order: "viewCount",
+        maxResults: 12,
+      });
+      const videoIds = response.data.items.map((item) => item.id.videoId);
+
+      const videoDetailsResponse = await YoutubeService.fetchVideos({
+        part: "contentDetails,snippet,statistics",
+        id: videoIds.join(","),
+      });
+
+      setVideos(videoDetailsResponse.data.items);
+    }catch(error){
+        console.log("error: "+ error);
+      }
+}
+
+useEffect (()=> {
+    fetchPlayList(section.snippet.channelId);
+  },[]);
+
+  return (
+    <div>
+      <h1 className={styles.video_header}>인기 동영상</h1>
+      <ul className={styles.video_list}>
+                  {videos.map((video) => (
+                    <li className={styles.video_item} key={video.id}>
+                      <div>
+                      <img
+                        className={styles.video_thumbnail}
+                        src={video.snippet.thumbnails.medium.url}
+                        alt={video.snippet.localized.title}
+                      />
+                      <p className={styles.video_duration}>{formatDuration(video.contentDetails.duration)}</p>
+                      </div>
+                      <div className={styles.video_description}>
+                        <p className={styles.video_title}>{video.snippet.localized.title}</p>
+                        <p className={styles.video_sub}>{formatHitCount(video.statistics.viewCount)}﹒{formatISO(video.snippet.publishedAt)}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+  </div>
+  );
+};
+
+export default PopularPlayList;
