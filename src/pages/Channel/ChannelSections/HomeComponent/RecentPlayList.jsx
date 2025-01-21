860f1d9f882505234e7f02a31b4285a47d530c7f
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-01-21 17:17:04 +0900

    feat: 채널 홈섹션 쇼츠, 비디오 분리 구현

--- src/pages/Channel/ChannelSections/HomeComponent/RecentPlayList.jsx
+++ src/pages/Channel/ChannelSections/HomeComponent/RecentPlayList.jsx
@@ -4,11 +4,13 @@ import styles from "./RecentPlayList.module.css";
 import { formatHitCount } from "../../../../utils/hit";
 import { formatDuration } from "../../../../utils/time";
 import { formatISO } from "../../../../utils/date";
+import {formatTotalTime} from "../../../../utils/totalTime";
 import { useNavigate } from "react-router-dom";
 import { PageEndPoints } from "../../../../constants/api";
 
 const RecentPlayList = ({ section }) => {
   const [videos, setVideos] = useState([]);
+  const [shorts, setShorts] = useState([]);
   const navigate = useNavigate();
 
   const fetchPlayList = async (playlistId) => {
@@ -19,7 +21,7 @@ const RecentPlayList = ({ section }) => {
         channelId: playlistId,
         regionCode: "KR",
         order: "date",
-        maxResults: 12,
+        maxResults: 24,
       });
       const videoIds = response.data.items.map((item) => item.id.videoId);
 
@@ -27,8 +29,16 @@ const RecentPlayList = ({ section }) => {
         part: "contentDetails,snippet,statistics",
         id: videoIds.join(","),
       });
-
-      setVideos(videoDetailsResponse.data.items);
+      const filteredVideos = videoDetailsResponse.data.items.filter((video) => {
+         const totalSeconds = formatTotalTime(video.contentDetails.duration);
+        return totalSeconds > 60;
+      });
+      setVideos(filteredVideos);
+      const filteredShorts = videoDetailsResponse.data.items.filter((video) => {
+        const totalSeconds = formatTotalTime(video.contentDetails.duration);
+       return totalSeconds <= 60;
+     });
+     setShorts(filteredShorts);
     } catch (error) {
       console.log("error: " + error);
     }
@@ -40,43 +50,82 @@ const RecentPlayList = ({ section }) => {
     },
     [navigate]
   );
-
+  const handleShortsClick = useCallback(
+    (id) => {
+      navigate(`/shorts/${id}`);
+    },
+    [navigate]
+  );
   useEffect(() => {
     fetchPlayList(section.snippet.channelId);
   }, []);
 
   return (
     <div>
-      <h1 className={styles.video_header}>동영상</h1>
-      <ul className={styles.video_list}>
-        {videos.map((video) => (
-          <li
-            className={styles.video_item}
-            key={video.id}
-            onClick={() => handleClick(video.id)}
-          >
-            <div>
-              <img
-                className={styles.video_thumbnail}
-                src={video.snippet.thumbnails.medium.url}
-                alt={video.snippet.localized.title}
-              />
-              <p className={styles.video_duration}>
-                {formatDuration(video.contentDetails.duration)}
-              </p>
-            </div>
-            <div className={styles.video_description}>
-              <p className={styles.video_title}>
-                {video.snippet.localized.title}
-              </p>
-              <p className={styles.video_sub}>
-                {formatHitCount(video.statistics.viewCount)}﹒
-                {formatISO(video.snippet.publishedAt)}
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <h1 className={styles.video_header}>동영상</h1>
+        <ul className={styles.video_list}>
+          {videos.map((video) => (
+            <li
+              className={styles.video_item}
+              key={video.id}
+              onClick={() => handleClick(video.id)}
+            >
+              <div>
+                <img
+                  className={styles.video_thumbnail}
+                  src={video.snippet.thumbnails.medium.url}
+                  alt={video.snippet.localized.title}
+                />
+                <p className={styles.video_duration}>
+                  {formatDuration(video.contentDetails.duration)}
+                </p>
+              </div>
+              <div className={styles.video_description}>
+                <p className={styles.video_title}>
+                  {video.snippet.localized.title}
+                </p>
+                <p className={styles.video_sub}>
+                  {formatHitCount(video.statistics.viewCount)}﹒
+                  {formatISO(video.snippet.publishedAt)}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h1 className={styles.video_header}>Shorts</h1>
+        <ul className={styles.video_list}>
+          {shorts.map((video) => (
+            <li
+              className={styles.shorts_item}
+              key={video.id}
+              onClick={() => handleShortsClick(video.id)}
+            >
+              <div>
+                <img
+                  className={styles.shorts_thumbnail}
+                  src={video.snippet.thumbnails.medium.url}
+                  alt={video.snippet.localized.title}
+                />
+                <p className={styles.shorts_duration}>
+                  {formatDuration(video.contentDetails.duration)}
+                </p>
+              </div>
+              <div className={styles.video_description}>
+                <p className={styles.video_title}>
+                  {video.snippet.localized.title}
+                </p>
+                <p className={styles.video_sub}>
+                  {formatHitCount(video.statistics.viewCount)}﹒
+                  {formatISO(video.snippet.publishedAt)}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
